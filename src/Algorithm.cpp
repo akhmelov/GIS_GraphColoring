2827165c92ea27c59828ab49166940cca9e9f38e
akhmelov <akhmelow@gmail.com>
2016-03-30 15:50:18 +0200

    corrected readbility in terminal

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -74,7 +74,7 @@ GraphColoring Algorithm::algorithmFull(Graph &graph, GraphColoring &graphColorin
 
         /*cout << "Justice coloring false lastColor: " << lastPositionOfPresentColors
             << " max(" << maxPositionVertix << "): "<< mostUsedColor << "  min(" << minPositionVertix << "): "<< lessUsedColor << endl;*/
-        cout << "Justice coloring false change color for  " << "[" << maxPositionVertix << "] from " << mostUsedColor << " to " <<  graphColoring[maxPositionVertix] << endl;
+        cout << "Justice coloring false change color for vertix " << "" << maxPositionVertix << " from " << mostUsedColor << " to " <<  graphColoring[maxPositionVertix] << endl;
 
     }
     //end justice coloring
